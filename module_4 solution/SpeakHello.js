c4dafcbff70b6423167253e941521d9620632d1f
IndraP24 <64627762+IndraP24@users.noreply.github.com>
2020-07-13 00:36:24 +0530

    Add files via upload

---
+++ module_4 solution/SpeakHello.js
@@ -0,0 +1,15 @@
+
+
+(function (window) {
+	var helloSpeaker = new Object();
+	var speakWord = "Hello";
+
+	helloSpeaker.speak = function (name) {
+	  console.log(speakWord + " " + name);
+	};
+	
+	window.helloSpeaker = helloSpeaker;
+
+})(window);
+
+
